69157bb8eff31db6823e1f74865fb2a9b7cc4340
so0601pk <youhami9@gmail.com>
2021-08-04 07:25:15 +0900

    Controllerを編集しました

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,7 +2,9 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class Admin extends Authenticatable
 {
