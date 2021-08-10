32de4204f6c012ff9e26c3b75030d6f86e7f2a13
so0601pk <youhami9@gmail.com>
2021-08-10 16:10:18 +0900

    Admin、Userコントローラーを作成

--- resources/views/admin/top.blade.php
+++ resources/views/admin/top.blade.php
@@ -1,3 +1,14 @@
 <?php
+require 'vendor/autoload.php';
+use Carbon\Carbon;
+
+$dt = Carbon::createFromDate(); //今の時間を取得
+
+function renderCalendar($dt){
+    $dt->timezone = 'Asia/Tokyo'; //日本時刻で表示
+    echo $dt;
+}
+
+renderCalendar($dt);
 
 ?>
