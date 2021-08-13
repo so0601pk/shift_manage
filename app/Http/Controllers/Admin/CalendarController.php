0c27d5c19e5c761e74a0478668b71b972c7ad43b
so0601pk <youhami9@gmail.com>
2021-08-13 15:21:57 +0900

    CalendarViewコントローラーを作成しました

---
+++ app/Http/Controllers/Admin/CalendarController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+//CalendarViewクラスを使用するので読み込む
+use App\Calendar\CalendarView;
+
+class CalendarController extends Controller
+{
+    //
+    public function show(){
+
+		$calendar = new CalendarView(time());
+
+		return view('calendar', [
+			"calendar" => $calendar
+		]);
+	}
+}
