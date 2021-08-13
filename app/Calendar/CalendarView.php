7a6125e25998b0ee627aa25f9b9ee2f98b4680d8
so0601pk <youhami9@gmail.com>
2021-08-13 17:17:34 +0900

    calendarcontrollerへのgetWeeks関数を追加、calendarweekクラスの作成

--- app/Calendar/CalendarView.php
+++ app/Calendar/CalendarView.php
@@ -39,4 +39,36 @@ class CalendarView {
 		$html[] = '</div>';
 		return implode("", $html);
 	}
+
+    /**
+	 * 週の情報を取得する
+	 */
+    protected function getWeeks(){
+		$weeks = [];
+
+		//初日
+		$firstDay = $this->carbon->copy()->firstOfMonth();
+
+		//月末まで
+		$lastDay = $this->carbon->copy()->lastOfMonth();
+
+		//1週目
+		$week = new CalendarWeek($firstDay->copy());
+		$weeks[] = $week;
+
+		//作業用の日
+		$tmpDay = $firstDay->copy()->addDay(7)->startOfWeek();
+
+		//月末までループさせる
+		while($tmpDay->lte($lastDay)){
+			//週カレンダーViewを作成する
+			$week = new CalendarWeek($tmpDay, count($weeks));
+			$weeks[] = $week;
+
+            //次の週=+7日する
+			$tmpDay->addDay(7);
+		}
+
+		return $weeks;
+	}
 }
