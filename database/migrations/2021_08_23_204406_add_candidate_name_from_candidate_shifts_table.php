ca71c79b10a46c3023655cd41d771bf341e94a4a
so0601pk <youhami9@gmail.com>
2021-08-23 20:48:51 +0900

    candidate_shiftテーブルのカラムをIntからStrへ変更

---
+++ database/migrations/2021_08_23_204406_add_candidate_name_from_candidate_shifts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCandidateNameFromCandidateShiftsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('candidate_shifts', function (Blueprint $table) {
+            //
+            $table->string('candidate_name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('candidate_shifts', function (Blueprint $table) {
+            //
+        });
+    }
+}
