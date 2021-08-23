59cf9acef4c57bbabfa203470005ed46c235e4b1
so0601pk <youhami9@gmail.com>
2021-08-23 09:12:00 +0900

    Applyコントローラーとapplyのviewを編集

--- app/Http/Controllers/User/ApplyController.php
+++ app/Http/Controllers/User/ApplyController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+//DBファサード使用のために記述
+use Illuminate\Support\Facades\DB;
+
 class ApplyController extends Controller
 {
     //
